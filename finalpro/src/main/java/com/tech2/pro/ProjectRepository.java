5beb8f7554b902b909b0e3fcb9a05c1b44b79795
tech <techtechnova131@gmail.com>
2025-09-13 00:07:57 +0530

    Delete finalpro directory

--- finalpro/src/main/java/com/tech2/pro/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tech2.pro;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-    List<Project> findByUser(User user);
-    
-}
